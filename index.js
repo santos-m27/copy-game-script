50cdfeb04d2ee38e0808e684723253ba99bd1093
santos-m27 <santosm2710@gmail.com>
2025-05-08 20:20:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const axios = require('axios');
-const multer = require('multer');
 const app = express();
 
 app.use(express.json());
