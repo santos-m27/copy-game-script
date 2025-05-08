0c3be5b38df38ef5b3935aeefcc550a4c67c7f2b
santos-m27 <santosm2710@gmail.com>
2025-05-08 20:15:51 +0100

    Update Node.js

--- Node.js
+++ Node.js
@@ -16,7 +16,7 @@ app.post('/send-to-discord', async (req, res) => {
     formData.append('file', fs.createReadStream(filename));
 
     try {
-        await axios.post('YOUR_DISCORD_WEBHOOK_URL', formData, {
+        await axios.post('https://discord.com/api/webhooks/1362192718218006580/NeJsmRKwktwr6jzvGcW7fsofLIoOGoSvReQfjKkXongWIZabiZIAppiNX5i_7s2m9piL', formData, {
             headers: formData.getHeaders()
         });
         res.send('Sent successfully');
